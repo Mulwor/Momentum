2866cd6124c9c71fb9f4fa0cfe170618bb8e3802
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-12-07 17:03:54 +0300

    feat: logic for time and date

--- js/time.js
+++ js/time.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector('.time');
+const calendar = document.querySelector('.date');
 
 function showTime() {
     let currentDatetime = new Date();
@@ -7,7 +8,23 @@ function showTime() {
     let currentTime = currentDatetime.toLocaleTimeString();
     // Свойство: .textContext - хранит в себе текстовое содержимое элемента.
     clock.textContent = currentTime;
+
+    showDate()
     setTimeout(showTime, 1000)
+};
+
+function showDate() {
+    let currentDatetime = new Date();
+    let options = {
+        // https://learn.javascript.ru/intl
+        weekday: 'long',
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+    };
+    let currentDate = currentDatetime.toLocaleDateString(['ru-RU', 'en-US'], options);
+    // en-US => В американском английском используется порядок месяц-день-год
+    calendar.textContent = currentDate;
 };
 
 showTime();
